c0bedb2593f4c0ade63f32b2573071b3e7f59875
pradeepisro <pradeepisro49@gmail.com>
2019-05-20 14:54:15 +0530

    Revert "[EXPERIMENTS] Added atomic example"
    This reverts commit ea8575149bb605fb82d2cc320317fa42ab0c86b6.

--- Experiments/atomic/atomic_cuda.cu
+++
@@ -1,8 +0,0 @@
-#include "atomic_cuda.cuh"
-
-__global__ void atomic_cuda(int *d_bins, const int *d_in, const int BIN_COUNT) {
-    int myId = threadIdx.x + blockDim.x * blockIdx.x;
-    int myItem = d_in[myId];
-    int myBin = myItem % BIN_COUNT;
-    atomicAdd(&(d_bins[myBin]), 1);
-}
--- Experiments/atomic/atomic_cuda.cuh
+++
@@ -1,3 +0,0 @@
-#include <cuda_runtime.h>
-
-extern __global__ void atomic_cuda(int *d_bins, const int *d_in, const int BIN_COUNT);
