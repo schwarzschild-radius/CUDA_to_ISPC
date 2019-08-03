9b954950e9b61b349c6779bab59eb3c5c587eb1d
Pradeep Kumar <pradeepisro49@gmail.com>
2019-08-03 18:39:05 +0530

    [EXPERIMENTS] Updated cuda utils and removed gputimer header

--- Experiments/utils/cuda_utils.cuh
+++ Experiments/utils/cuda_utils.cuh
@@ -1,24 +1,18 @@
-#include "gputimer.h"
-
 #define cudaCheck(stmt)                                                        \
-  {                                                                            \
-    cudaError_t err = stmt;                                                    \
-    if (err != cudaSuccess) {                                                  \
-      std::cerr << "Failed to run " << #stmt << std::endl;                     \
-      std::cerr << cudaGetErrorString(err) << std::endl;                       \
-    }                                                                          \
-  }
+    {                                                                          \
+        cudaError_t err = stmt;                                                \
+        if (err != cudaSuccess) {                                              \
+            std::cerr << __LINE__ << ":Failed to run " << #stmt << std::endl;               \
+            std::cerr << cudaGetErrorString(err) << std::endl;                 \
+        }                                                                      \
+    }
 
-#define cudaCheckLaunch(kernel, grid, block, ...)                              \
-  {                                                                            \
-    GpuTimer timer;                                                            \
-    timer.Start();                                                             \
-    kernel<<<grid, block>>>(__VA_ARGS__);                                      \
-    timer.Stop();                                                              \
-    std::cout << #kernel << ":\nTime elapsed: " << timer.Elapsed() << "\n";    \
-    cudaError_t err = cudaGetLastError();                                      \
-    if (err != cudaSuccess) {                                                  \
-      std::cerr << "Failed to run " << #kernel << std::endl;                   \
-      std::cerr << cudaGetErrorString(err) << std::endl;                       \
-    }                                                                          \
-  }
+#define cudaCheckLaunch(kernel, grid, block, shared_memory, ...)                 \
+    {                                                                          \
+        kernel<<<grid, block, shared_memory>>>(__VA_ARGS__);                   \
+        cudaError_t err = cudaGetLastError();                                  \
+        if (err != cudaSuccess) {                                              \
+            std::cerr << "Failed to run " << #kernel << std::endl;             \
+            std::cerr << cudaGetErrorString(err) << std::endl;                 \
+        }                                                                      \
+    }
